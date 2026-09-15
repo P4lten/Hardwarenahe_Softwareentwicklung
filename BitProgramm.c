d1a103f995f5aeb3ae52435eba2b79b4d4e59b8a
Palten <altenbergerp@gmail.com>
2026-09-15 18:58:08 +0200

    ADDED: BitProgramm + Script in gitignore

---
+++ BitProgramm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    puts("Diese Binaerzahl 0100000110111101 als");
+    printf("Integer: %d\n", 0b0100000110111101);
+    printf("Hexadecimal: %x\n", 0b0100000110111101);
+
+    return 0;
+}
